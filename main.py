cdf6e6f5564a40ccc55d9c7bada7f26a654a130e
UNREALre <avpmanager@gmail.com>
2020-05-11 19:33:23 +0400

    encoding fix#2

--- main.py
+++ main.py
@@ -12,17 +12,21 @@ config = confuse.Configuration('ProductsKK')
 
 bot = telebot.TeleBot(config['tg']['key'].get())
 
+
 @bot.message_handler(commands=['start'])
 def startMessage(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
+
 @bot.message_handler(content_types=['text'])
 def sendText(message):
-    if message.text.lower() == 'привет':
+    userMessage = message.text.lower().encode('utf-8')
+    if userMessage == 'привет':
         bot.send_message(message.chat.id, 'Привет!')
-    elif message.text.lower() == 'мяу':
+    elif userMessage == 'мяу':
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAINHV65UcH0-Qf80HIztYDHafoxf6F6AAJ1AAPZvGoaxRDGAz1iuPEZBA')
     else:
-        bot.send_message(message.chat.id, 'Зачем ты мне написал мне - ' + message.text.lower() + ' ?')
+        bot.send_message(message.chat.id, 'Зачем ты мне написал мне - ' + userMessage + ' ?')
+
 
 bot.polling()
