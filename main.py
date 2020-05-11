9fe0862a2b2d42b1af936f702b29ba3ec64dd838
UNREALre <avpmanager@gmail.com>
2020-05-11 18:17:39 +0400

    Telegram testing

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello")
+import os
+import confuse
+import telebot
+
+projectRoot = os.path.dirname(os.path.dirname(__file__) + "/ProductsKK")
+os.environ["PRODUCTSKKDIR"] = projectRoot
+
+config = confuse.Configuration('ProductsKK')
+
+bot = telebot.TeleBot(config['tg']['key'].get())
+
+@bot.message_handler(commands=['start'])
+def startMessage(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+@bot.message_handler(content_types=['text'])
+def sendText(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет!')
+    elif message.text.lower() == 'мяу':
+        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAINHV65UcH0-Qf80HIztYDHafoxf6F6AAJ1AAPZvGoaxRDGAz1iuPEZBA')
+    else:
+        bot.send_message(message.chat.id, 'Что-что ?')
+
+bot.polling()
